65407200a56f4789249986f5c44a98f935ad2d52
Harry <quangvinh050293@gmail.com>
2024-11-22 14:25:10 +0700

    initial grpc

--- gateway/http_handler.go
+++ gateway/http_handler.go
@@ -1,16 +1,36 @@
 package main
 
-import "net/http"
+import (
+	"github.com/hawful70/common"
+	pb "github.com/hawful70/common/api"
+	"net/http"
+)
 
 type handler struct {
+	client pb.OrderServiceClient
 }
 
-func NewHandler() *handler {
-	return &handler{}
+func NewHandler(client pb.OrderServiceClient) *handler {
+	return &handler{client}
 }
 
 func (h *handler) registerRoutes(mux *http.ServeMux) {
 	mux.HandleFunc("POST /api/customers/{customerID}/orders", h.handleCreateOrder)
 }
 
-func (h *handler) handleCreateOrder(w http.ResponseWriter, r *http.Request) {}
+func (h *handler) handleCreateOrder(w http.ResponseWriter, r *http.Request) {
+	customerId := r.PathValue("customerID")
+	var items []*pb.ItemsWithQuantity
+	if err := common.ReadJSON(r, &items); err != nil {
+		common.WriteError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	_, err := h.client.CreateOrder(r.Context(), &pb.CreateOrderRequest{
+		CustomerID: customerId,
+		Items:      items,
+	})
+	if err != nil {
+		return
+	}
+}
--- gateway/main.go
+++ gateway/main.go
@@ -2,18 +2,36 @@ package main
 
 import (
 	"github.com/hawful70/common"
+	pb "github.com/hawful70/common/api"
 	_ "github.com/joho/godotenv/autoload"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"log"
 	"net/http"
 )
 
 var (
-	httpAddr = common.EnvString("HTTP_ADDR", ":8080")
+	httpAddr         = common.EnvString("HTTP_ADDR", ":8080")
+	orderServiceAddr = common.EnvString("ORDER_SERVICE_ADDR", ":2000")
 )
 
 func main() {
+	conn, err := grpc.NewClient(orderServiceAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to dial server: %v", err)
+	}
+	defer func(conn *grpc.ClientConn) {
+		err := conn.Close()
+		if err != nil {
+			log.Fatalf("Failed to close connection: %v", err)
+		}
+	}(conn)
+
+	log.Printf("Dialing  at %s", orderServiceAddr)
+	c := pb.NewOrderServiceClient(conn)
+
 	mux := http.NewServeMux()
-	handler := NewHandler()
+	handler := NewHandler(c)
 	handler.registerRoutes(mux)
 
 	log.Printf("Starting HTTP server at %s", httpAddr)
