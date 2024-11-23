1c6371bbfd12b3d306234f9f81f22830fd45ccf2
Harry <quangvinh050293@gmail.com>
2024-11-23 13:43:45 +0700

    adopting order with consul discovery service

--- orders/main.go
+++ orders/main.go
@@ -3,16 +3,55 @@ package main
 import (
 	"context"
 	"github.com/hawful70/common"
+	"github.com/hawful70/common/discovery"
+	"github.com/hawful70/common/discovery/consul"
 	"google.golang.org/grpc"
 	"log"
 	"net"
+	"time"
 )
 
 var (
-	grpcAddr = common.EnvString("GRPC_ADDR", "localhost:2000")
+	serviceName = "orders"
+	grpcAddr    = common.EnvString("GRPC_ADDR", "localhost:2000")
+	consulAddr  = common.EnvString("CONSUL_ADDR", "localhost:8500")
+	amqpUser    = common.EnvString("RABBITMQ_USER", "guest")
+	amqpPass    = common.EnvString("RABBITMQ_PASS", "guest")
+	amqpHost    = common.EnvString("RABBITMQ_HOST", "localhost")
+	amqpPort    = common.EnvString("RABBITMQ_PORT", "5672")
+	mongoUser   = common.EnvString("MONGO_DB_USER", "root")
+	mongoPass   = common.EnvString("MONGO_DB_PASS", "example")
+	mongoAddr   = common.EnvString("MONGO_DB_HOST", "localhost:27017")
+	jaegerAddr  = common.EnvString("JAEGER_ADDR", "localhost:4318")
 )
 
 func main() {
+	registry, err := consul.NewRegistry(consulAddr, serviceName)
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+	instanceID := discovery.GenerateInstanceID(serviceName)
+	if err := registry.Register(ctx, instanceID, serviceName, grpcAddr); err != nil {
+		panic(err)
+	}
+
+	go func() {
+		for {
+			if err := registry.HealthCheck(instanceID, serviceName); err != nil {
+				log.Fatal("failed to health check")
+			}
+			time.Sleep(time.Second * 1)
+		}
+	}()
+
+	defer func(registry *consul.Registry, ctx context.Context, instanceID string, serviceName string) {
+		err := registry.Deregister(ctx, instanceID, serviceName)
+		if err != nil {
+			log.Fatal("failed to deregister service")
+		}
+	}(registry, ctx, instanceID, serviceName)
 
 	grpcServer := grpc.NewServer()
 	l, err := net.Listen("tcp", grpcAddr)
@@ -28,6 +67,7 @@ func main() {
 
 	store := NewStore()
 	svc := NewService(store)
+
 	NewGRPCHandler(grpcServer)
 
 	svc.CreateOrder(context.Background())
